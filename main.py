b14f918738cff3834ecb93b82e2ba90d208b27ec
GiganticCat <harveyzhang612@gmail.com>
2024-11-23 16:09:33 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 import pygame
+import sys
 
-pygame.init()
-pygame.display.set_caption('Battle Cats Animation Viewer')
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-dt = 0
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 4)
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((680, 360))
+        self.clock = pygame.time.Clock()
+        self.gameStateManager = GameStateManager('start')
+        self.start = Start(self.screen, self.gameStateManager)
+        self.level = Level(self.screen, self.gameStateManager)
+        
+        self.states = {'start':self.start, 'level':self.level}
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.states[self.gameStateManager.getState()].run()
+            pygame.display.update()
+            self.clock.tick(60)
 
-while running:
+class Level():
+    def __init__(self, display, gameStateManager):
+        self.display = display
+        self.gameStateManager = gameStateManager
+    def run(self):
+        self.display.fill('blue')
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000
-    print(clock.tick(60))
+class Start():
+    def __init__(self, display, gameStateManager):
+        self.display = display
+        self.gameStateManager = gameStateManager
+    def run(self):
+        self.display.fill('red')
 
-pygame.quit()
+class GameStateManager():
+    def __init__(self, currentState):
+        self.currentState = currentState
+    def getState(self):
+        return self.currentState
+    def setState(self, state):
+        self.currentState = state
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
